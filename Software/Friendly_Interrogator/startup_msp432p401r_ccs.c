a79ab4fa356fe186d102d900932ceb7ba5158cf8
Eric Meyers <eric.meyers12@gmail.com>
2016-04-07 17:25:10 -0500

    Commit software

--- Software/Friendly_Interrogator/startup_msp432p401r_ccs.c
+++ Software/Friendly_Interrogator/startup_msp432p401r_ccs.c
@@ -57,6 +57,7 @@ extern unsigned long __STACK_END;
 /* External declarations for the interrupt handlers used by the application. */
 extern void TA0_0_IRQHandler(void);
 extern void TA1_0_IRQHandler(void);
+void ADC14_IRQHandler(void);
 /* To be added by user */
 
 
@@ -108,7 +109,7 @@ void (* const interruptVectors[])(void) =
     defaultISR,                             /* EUSCIB1 ISR               */
     defaultISR,                             /* EUSCIB2 ISR               */
     defaultISR,                             /* EUSCIB3 ISR               */
-    defaultISR,                             /* ADC14 ISR                 */
+	ADC14_IRQHandler,                             /* ADC14 ISR                 */
     defaultISR,                             /* T32_INT1 ISR              */
     defaultISR,                             /* T32_INT2 ISR              */
     defaultISR,                             /* T32_INTC ISR              */
