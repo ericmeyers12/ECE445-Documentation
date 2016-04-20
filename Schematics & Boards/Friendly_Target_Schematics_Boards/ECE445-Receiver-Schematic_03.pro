7fed8e2a14278709d03803ba4d5a60f038bba1cc
Eric Meyers <eric.meyers12@gmail.com>
2016-04-19 22:47:14 -0500

    Final Report Template

---
+++ Schematics & Boards/Friendly_Target_Schematics_Boards/ECE445-Receiver-Schematic_03.pro
@@ -0,0 +1,25 @@
+EAGLE AutoRouter Statistics:
+
+Job           : /Users/Eric_Meyers/Documents/Senior-Year-2nd-Semester-(SP16)/ECE445/ECE445-Documentation/Schematics & Boards/Friendly_Target_Schematics_Boards/ECE445-Receiver-Schematic.brd
+
+Start at      : 12:25:32 (4/11/16)
+End at        : 18:00:00 (12/31/69)
+Elapsed time  : 00:00:00
+
+Signals       :    35   RoutingGrid: 6.25 mil  Layers: 2
+Connections   :    69   predefined:  69 ( 0 Vias )
+
+Router memory :   0
+
+Passname          :          
+
+Time per pass     :  00:00:00
+Number of Ripups  :         0
+max. Level        :         0
+max. Total        :         0
+
+Routed            :         0
+Vias              :         0
+Resolution        :   100.0 %
+
+Final             : 
---
+++ Software/msp432p401_ta1_05/Debug/subdir_vars.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+../msp432p401_ta1_05.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+OBJS += \
+./msp432p401_ta1_05.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+C_DEPS += \
+./msp432p401_ta1_05.pp \
+./startup_msp432p401r_ccs.pp \
+./system_msp432p401r.pp 
+
+C_DEPS__QUOTED += \
+"msp432p401_ta1_05.pp" \
+"startup_msp432p401r_ccs.pp" \
+"system_msp432p401r.pp" 
+
+OBJS__QUOTED += \
+"msp432p401_ta1_05.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_SRCS__QUOTED += \
+"../msp432p401_ta1_05.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
